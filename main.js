95f0898f58de185f5365a5f69b92ebf04e68b2fd
jinsumin <suminjin@naver.com>
2021-12-13 03:45:19 +0900

    add img files

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log("navbarHeight : " + navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
@@ -16,53 +14,38 @@ document.addEventListener("scroll", () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu /
-  addEventListener("click", (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-      return;
-    }
-    scrollIntoView(link);
-  });
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove("open");
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
 
 // Handle click on "contact me" button on home
-const homeContactButton = document.querySelector(".home__contact");
-homeContactButton.addEventListener("click", () => {
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
-const about = document.querySelector(".about__container");
-const aboutHeight = about.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
-  about.style.opacity = 1 - (window.scrollY - homeHeight) / aboutHeight;
-});
-
-const skills = document.querySelector(".skills__container");
-const skillsHeight = skills.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
-  skills.style.opacity =
-    1 - (window.scrollY - (homeHeight + aboutHeight)) / skillsHeight;
-});
-
-const work = document.querySelector(".work__container");
-const workHeight = work.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
-  work.style.opacity =
-    1 -
-    (window.scrollY - (homeHeight + aboutHeight + skillsHeight)) / workHeight;
 });
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("click", () => {
+document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
     arrowUp.classList.add("visible");
   } else {
@@ -76,10 +59,10 @@ arrowUp.addEventListener("click", () => {
 });
 
 // Projects
-const workButtonContainer = document.querySelector(".work__categories");
+const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
-workButtonContainer.addEventListener("click", (e) => {
+workBtnContainer.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
@@ -87,9 +70,10 @@ workButtonContainer.addEventListener("click", (e) => {
 
   // Remove selection from the previous item and select the new one.
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove('selected');
-  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
